a4291178a7ad1aa5cb1e7bf3a16bb6be4d102fb8
kingstar <admin@baks.dev>
2025-02-06 11:51:14 +0300

    Baks Development

--- Controller/Admin/QrcodeController.php
+++ Controller/Admin/QrcodeController.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,11 +26,16 @@ declare(strict_types=1);
 namespace BaksDev\Manufacture\Part\Controller\Admin;
 
 
+use BaksDev\Barcode\Writer\BarcodeFormat;
+use BaksDev\Barcode\Writer\BarcodeType;
+use BaksDev\Barcode\Writer\BarcodeWrite;
 use BaksDev\Core\Controller\AbstractController;
 use BaksDev\Core\Listeners\Event\Security\RoleSecurity;
 use BaksDev\Manufacture\Part\Entity\ManufacturePart;
 use chillerlan\QRCode\QRCode;
+use RuntimeException;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -46,15 +51,30 @@ final class QrcodeController extends AbstractController
     #[Route('/admin/manufacture/part/qrcode/{id}', name: 'admin.qrcode', methods: ['GET', 'POST'])]
     public function qrcode(
         Request $request,
-        #[MapEntity] ManufacturePart $ManufacturePart
+        #[MapEntity] ManufacturePart $ManufacturePart,
+        BarcodeWrite $BarcodeWrite
     ): Response
     {
-
         $data = sprintf('%s', $ManufacturePart->getId());
+
+        $barcode = $BarcodeWrite
+            ->text($data)
+            ->type(BarcodeType::QRCode)
+            ->format(BarcodeFormat::SVG)
+            ->generate(implode(DIRECTORY_SEPARATOR, ['barcode', 'test']));
+
+        if($barcode === false)
+        {
+            throw new RuntimeException('Barcode write error');
+        }
+
+        $QRCode = $BarcodeWrite->render();
+
+        $BarcodeWrite->remove();
 
         return $this->render(
             [
-                'qrcode' => (new QRCode())->render($data),
+                'qrcode' => $QRCode,
                 'item' => $ManufacturePart
             ]
         );
