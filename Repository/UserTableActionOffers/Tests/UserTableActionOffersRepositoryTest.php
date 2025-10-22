688f3c92d4f17a3251309f13b052441e4dff9fcc
MichelAngelo <baksdevelopmen@gmail.com>
2025-10-23 00:39:43 +0300

    Baks Development
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Repository/UserTableActionOffers/Tests/UserTableActionOffersRepositoryTest.php
+++ Repository/UserTableActionOffers/Tests/UserTableActionOffersRepositoryTest.php
@@ -1,17 +1,17 @@
 <?php
 /*
  *  Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,8 @@ namespace BaksDev\Manufacture\Part\Repository\UserTableActionOffers\Tests;
 use BaksDev\Manufacture\Part\Application\BaksDevManufacturePartApplicationBundle;
 use BaksDev\Manufacture\Part\Application\Type\Id\ManufactureApplicationUid;
 use BaksDev\Manufacture\Part\Repository\UserTableActionOffers\UserTableActionOffersInterface;
-use BaksDev\Manufacture\Part\Repository\UserTableActionOffers\UserTableActionOffersRepository;
 use BaksDev\Users\UsersTable\Type\Actions\Id\UsersTableActionsUid;
+use PHPUnit\Framework\Attributes\Group;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Attribute\When;
 
@@ -35,6 +35,7 @@ use Symfony\Component\DependencyInjection\Attribute\When;
  * @group manufacture-part
  */
 #[When(env: 'test')]
+#[Group('manufacture-part')]
 class UserTableActionOffersRepositoryTest extends KernelTestCase
 {
     public function testFindActionOffersByMain()
@@ -42,7 +43,7 @@ class UserTableActionOffersRepositoryTest extends KernelTestCase
         /** @var UserTableActionOffersInterface $repository */
         $repository = self::getContainer()->get(UserTableActionOffersInterface::class);
 
-        if (class_exists(BaksDevManufacturePartApplicationBundle::class))
+        if(class_exists(BaksDevManufacturePartApplicationBundle::class))
         {
             $result = $repository->findActionOffersByMain(new UsersTableActionsUid(ManufactureApplicationUid::ACTION_ID));
         }
