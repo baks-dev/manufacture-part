1e1f536957ab99baab9bb0a3449329618a22e65c
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-04 22:05:28 +0300

    Baks Development
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Repository/UserTableActionOffers/Tests/UserTableActionOffersRepositoryTest.php
+++ Repository/UserTableActionOffers/Tests/UserTableActionOffersRepositoryTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -44,8 +44,6 @@ class UserTableActionOffersRepositoryTest extends KernelTestCase
         {
             $result = $repository->findActionOffersByMain(new UsersTableActionsUid(ManufactureApplicationUid::ACTION_ID));
         }
-
-        //        dd($result);
 
         self::assertTrue(true);
     }
